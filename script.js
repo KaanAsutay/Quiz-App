33784c839afdb777ec236e9c12744e76df717b76
Kaan <kaanasutay10@gmail.com>
2024-04-19 21:23:38 +0300

    const questions

---
+++ script.js
@@ -0,0 +1,32 @@
+const questions = [
+
+    {
+        question: "Which is largest animal in the world?",
+        answer: [
+            { text: "Shark", correct: false },
+            { text: "Blue whale", correct: true },
+            { text: "Elephant", correct: false },
+            { text: "Giraffe", correct: false },
+        ]
+    },
+
+    {
+        question: "Which is the largest desert in the world?",
+        answer: [
+            { text: "Kalahari", correct: false },
+            { text: "Gobi", correct: false },
+            { text: "Sahara", correct: false },
+            { text: "Antartica", correct: true },
+        ]
+    },
+
+    {
+        question: "Which is the smallest continent in the world?",
+        answer: [
+            { text: "Asia", correct: false },
+            { text: "Australia", correct: true },
+            { text: "Artic", correct: false },
+            { text: "Africa", correct: false },
+        ]
+    }
+]
