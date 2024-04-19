bd2bd3a2a11da62398df46c976fdd9fa574075e4
Kaan <kaanasutay10@gmail.com>
2024-04-19 22:17:50 +0300

    function showQuestion

--- script.js
+++ script.js
@@ -44,3 +44,9 @@ function startQuiz() {
     nextButton.innerHTML = "Next";
     showQuestion();
 }
+
+function showQuestion() {
+    let currentQuestion = questions[currentQuestionIndex];
+    let questionNo = currentQuestionIndex + 1;
+    questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+}
